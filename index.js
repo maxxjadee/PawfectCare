9da7634bf525d951e79ca23125f1af4ec0c06c4e
maxxjadee <54099478+maxxjadee@users.noreply.github.com>
2025-05-04 18:28:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const enteredEmail = event.target[0].value;
+    const enteredPassword = event.target[1].value;
+
+    const savedEmail = localStorage.getItem('email');
+    const savedPassword = localStorage.getItem('password');
+    const savedName = localStorage.getItem('name');
+
+    if (enteredEmail === savedEmail && enteredPassword === savedPassword) {
+        localStorage.setItem("loggedInUser", savedName); // <- This line is key
+        window.location.href = "welcome.html";
+    } else {
+        alert("Invalid email or password.");
+    }
+});
